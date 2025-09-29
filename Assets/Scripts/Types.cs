a984447df1dc3821a870088718639fe56a334a81
Emanuele Villani <35426518+EmanueleVillani@users.noreply.github.com>
2025-09-29 14:30:01 +0200

    Inizio GHITUB
    aggiunto file

---
+++ Assets/Scripts/Types.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Tile {
+    public GameObject tileObject;
+    public Vector3 position;
+    public int bitValue;
+    public Tile(GameObject tileObject, Vector3 position, int bitValue) {
+        this.tileObject = tileObject;
+        this.position = position;
+        this.bitValue = bitValue;
+    }
+}
+
+[System.Serializable]
+public struct DirectionInfo {
+    public int bit;
+    public Vector2Int offset;
+    public DirectionInfo(int bit, int x, int y) {
+        this.bit = bit;
+        offset = new Vector2Int(x, y);
+    }
+}
